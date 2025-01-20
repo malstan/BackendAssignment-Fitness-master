73cc4d052fbc9b794b96cc6740b6e4889f894d66
Stanislav Malík <stanislavmalik@outlook.com>
2025-01-20 22:06:31 +0100

    updated database connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,7 +7,7 @@ import { Sequelize } from 'sequelize'
 import defineExercise from './exercise'
 import defineProgram from './program'
 
-const sequelize: Sequelize = new Sequelize('postgresql://localhost:5432/fitness_app', {
+const sequelize: Sequelize = new Sequelize('postgresql://postgres:password@localhost:5432/fitness_app', {
 	logging: false
 })
 
